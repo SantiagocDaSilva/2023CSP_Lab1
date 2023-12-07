cfacfbc43df77afbbe7fc6359b464493f9c4be8d
SantiagocDaSilva <santiago.dasilva@vtsd.com>
2023-12-07 10:07:36 -0500

    12/7/23 Lab

--- main.py
+++ main.py
@@ -33,21 +33,46 @@ def setupBox():
 
 # Code for 80 point version goes here
 def v80():
+    lines.speed(0)
     lines.showturtle()
     lines.penup()
     lines.goto(-490, -300)
     lines.pendown()
     lines.goto(490, -250)
+    startx = -490
+    starty = -300
+    endx = 490
+    endy = -300
 
-    for x in range(100):
-        lines.goto(-490, -300)
-        lines.goto(490, -210)
+    for x in range(50):
+        lines.goto(startx, starty)
+        lines.goto(endx, endy)
+        startx += 30
+        endy += 20
+        lines.goto(startx, starty)
+
 
 
 # Code for the 90 point version goes here
 def v90():
     # Calling the 80 point function - don't copy-paste from earlier method!!
     v80()
+    lines.speed(0)
+    lines.showturtle()
+    lines.penup()
+    lines.goto(490, -300)
+    lines.pendown()
+    lines.goto(490, -250)
+    startx = 490
+    starty = -300
+    endx = -490
+    endy = -300
+    for x in range(50):
+        lines.goto(startx, starty)
+        lines.goto(endx, endy)
+        startx += 30
+        endy += 20
+        lines.goto(startx, starty)
 
 
 # Code for the 100 point version here
